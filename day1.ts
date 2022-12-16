46b356c858d3c3da9a1b36c4bf10869f5f11a1e9
Peter Caisse <pcaisse@gmail.com>
2022-12-15 21:33:46 -0500

    Day 1 part 2 solution

--- day1.ts
+++ day1.ts
@@ -2,15 +2,17 @@
 
 import fs from "fs";
 
-const result = fs
+const add = (a: number, b: number) => a + b;
+
+const topCalorieElves = fs
   .readFileSync(process.stdin.fd, "utf-8")
   .split("\n\n")
   .map((snacks) =>
     snacks
       .split("\n")
       .map((s) => Number(s))
-      .reduce((total, x) => total + x, 0)
+      .reduce(add, 0)
   )
   .sort((a, b) => b - a);
 
-console.log(result[0]);
+console.log(topCalorieElves.slice(0, 3).reduce(add, 0));
